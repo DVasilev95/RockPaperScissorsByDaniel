d160b145eb0c97c0614dc0eff9ca6f042ce33f15
DVasilev95 <daniel.vasilev950905@gmail.com>
2023-04-07 23:05:29 +0300

    Update rockPaperScissors.js

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -4,35 +4,17 @@ function rockPaperScissors(playerTurn) {
     const rock = "Rock";
     const paper = "Paper";
     const scissors = "Scissors";
-
-    let playerRandomNumber = Math.floor(Math.random() * 3) + 1
-
-    switch (playerRandomNumber) {
-        case 1:
-            playerTurn = rock;
-            break;
-        case 2:
-            playerTurn = paper;;
-            break;
-        case 3:
-            playerTurn = scissors;
-            break;
-        default:
-            console.log('Invalid Input. Try Again...');
+    if (playerTurn === 'r' || playerTurn === 'rock') {
+        playerTurn = rock
+    } else if (playerTurn === 'p' || playerTurn === 'paper') {
+        playerTurn = paper
+    } else if (playerTurn === 's' || playerTurn === 'scissors') {
+        playerTurn = scissors
+    } else {
+        console.log('Invalid Input. Try Again...');
+        return;
     }
     console.log(`You choose ${playerTurn}`.blue);
-
-    // if (playerTurn === 'r' || playerTurn === 'rock') {
-    //     playerTurn = rock
-    // } else if (playerTurn === 'p' || playerTurn === 'paper') {
-    //     playerTurn = paper
-    // } else if (playerTurn === 's' || playerTurn === 'scissors') {
-    //     playerTurn = scissors
-    // } else {
-    //     console.log('Invalid Input. Try Again...');
-    //     return;
-    // }
-    // console.log(`You choose ${playerTurn}`.blue);
 
     let computerRandomNumber = Math.floor(Math.random() * 3) + 1
     let computerTurn = '';
@@ -57,7 +39,7 @@ function rockPaperScissors(playerTurn) {
     } else {
         console.log('This game was a draw!'.yellow);
     }
-    
+
 }
 rockPaperScissors();
 
